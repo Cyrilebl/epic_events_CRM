492d95421fd3ec091e1851ac81f1d2ffeffcd49c
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-02-21 23:41:17 +0100

    PyJWT token implementation

---
+++ src/controllers/token.py
@@ -0,0 +1,38 @@
+import os
+import jwt
+import datetime
+from dotenv import load_dotenv
+
+from src.views.error_message import ErrorMessage
+
+
+load_dotenv()
+
+SECRET_KEY = os.getenv("SECRET_KEY")
+
+
+class Token:
+    def __init__(self):
+        self.error_message = ErrorMessage()
+
+    def generate_token(self, user_id, role_name):
+        payload = {
+            "sub": str(user_id),
+            "role": role_name,
+            "exp": datetime.datetime.now(datetime.timezone.utc)
+            + datetime.timedelta(hours=1),
+        }
+        token = jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+        return token
+
+    def verify_token(self, token):
+        try:
+            payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+            return payload["sub"], payload["role"]
+
+        except jwt.ExpiredSignatureError:
+            self.error_message.token_expired()
+        except jwt.InvalidTokenError:
+            self.error_message.invalid_token()
+
+        return None, None
