22c10096e12916778611b66491b56e3f97601d86
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-02-23 23:05:57 +0100

    add new files

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,6 +1,7 @@
 from .menu import Menu
-from .user_view import UserView
+from .user_prompt import UserPrompt
+from .formatter import Formatter
 from .success_message import SuccessMessage
 from .error_message import ErrorMessage
 
-__all__ = [Menu, UserView, SuccessMessage, ErrorMessage]
+__all__ = [Menu, UserPrompt, Formatter, SuccessMessage, ErrorMessage]
