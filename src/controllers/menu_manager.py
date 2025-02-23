9002a7e6e243c289975d336a86fd7f0fb82fb4c6
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-02-23 23:04:47 +0100

    display data on terminal

--- src/controllers/menu_manager.py
+++ src/controllers/menu_manager.py
@@ -1,9 +1,11 @@
-from src.views.menu import Menu
+from src.views import Menu, Formatter
+from src.models import Contract, Event, User
 
 
 class MenuManager:
     def __init__(self):
         self.menu = Menu()
+        self.formatter = Formatter()
 
     def show_menu(self, role):
         match role:
@@ -13,3 +15,16 @@ class MenuManager:
                 return self.menu.commercial()
             case "support":
                 return self.menu.support()
+
+    def show_data(self, session, user_input):
+        match user_input:
+            case 1:
+                clients_data = session.query(User).all()
+                return self.formatter.format_clients(clients_data)
+            case 2:
+                self.session.query(Contract).all()
+                return self.formatter.format_contract(clients_data)
+
+            case 3:
+                self.session.query(Event).all()
+                return self.formatter.format_event(clients_data)
