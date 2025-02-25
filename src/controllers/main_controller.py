72af0c64b0b91e326e18071198b2b60185680613
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-02-25 21:35:04 +0100

    add manager actions

--- src/controllers/main_controller.py
+++ src/controllers/main_controller.py
@@ -2,6 +2,7 @@ from .user_manager import UserManager
 from .menu_manager import MenuManager
 from .token import Token
 from src.models import init_db
+from src.views import SuccessMessage, UserInteraction
 
 
 class MainController:
@@ -10,6 +11,8 @@ class MainController:
         self.manager = UserManager()
         self.menu_manager = MenuManager()
         self.token = Token()
+        self.success_message = SuccessMessage()
+        self.user_interaction = UserInteraction()
 
     def run(self):
         # Login
@@ -17,9 +20,14 @@ class MainController:
         user_id, role = self.token.verify_token(token)
 
         # Menu
-        user_input = self.menu_manager.show_menu(role)
-        self.menu_manager.show_data(self.session, user_input)
-
+        # if not user:
+        #     return "Access denied. Please log in."
+        # else
+        while True:
+            user_input = self.menu_manager.show_menu(role)
+            self.menu_manager.show_data(self.session, user_input)
+            self.menu_manager.user_is_manager(self.session, user_input)
+            self.user_interaction.return_to_menu()
         # user = "test"
         # if user.has_permission("assign_support"):
         #     events_without_assigned_support = (
