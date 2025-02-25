c4c3423a5b3965e76829b739fe59ab8fa3fb6c36
Cyril Leblanc <cyril.leblanc@outlook.com>
2025-02-25 21:38:24 +0100

    add edit_user

--- src/controllers/user_manager.py
+++ src/controllers/user_manager.py
@@ -1,20 +1,21 @@
 from .token import Token
 from src.models import User, Role
-from src.views import Menu, UserPrompt, SuccessMessage, ErrorMessage
+from src.views import Menu, UserPrompt, Formatter, SuccessMessage, ErrorMessage
 
 
 class UserManager:
     def __init__(self):
         self.token = Token()
         self.menu = Menu()
-        self.user_view = UserPrompt()
+        self.user_prompt = UserPrompt()
+        self.formatter = Formatter()
         self.success_message = SuccessMessage()
         self.error_message = ErrorMessage()
 
     def login(self, session):
         self.menu.login()
         while True:
-            email, password = self.user_view.login()
+            email, password = self.user_prompt.login()
 
             user = session.query(User).filter_by(email=email).first()
 
@@ -26,7 +27,9 @@ class UserManager:
             return token
 
     def create_user(self, session):
-        first_name, last_name, email, password, role_name = self.user_view.create_user()
+        first_name, last_name, email, password, role_name = (
+            self.user_prompt.create_user()
+        )
         role = session.query(Role).filter_by(name=role_name).first()
 
         user = User(
@@ -40,6 +43,26 @@ class UserManager:
         session.add(user)
         session.commit()
 
-        self.success_message.confirm_registration(
-            f"{user.last_name.title()} {user.first_name.title()} ({user.role_name})"
+        self.success_message.confirm_action(
+            f"{user.last_name.title()} {user.first_name.title()} ({user.role_name})",
+            "created",
+        )
+
+    def edit_user(self, session, user):
+        self.formatter.format_one_user(user)
+        self.user_prompt.edit_user(user)
+        session.commit()
+
+        self.success_message.confirm_action(
+            f"{user.last_name.title()} {user.first_name.title()} ({user.role_name})",
+            "edited",
+        )
+
+    def delete_user(self, session, user):
+        session.delete(user)
+        session.commit()
+
+        self.success_message.confirm_action(
+            f"{user.last_name.title()} {user.first_name.title()} ({user.role_name})",
+            "deleted",
         )
